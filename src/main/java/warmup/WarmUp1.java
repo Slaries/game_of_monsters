91f1e56ee1ff4c0765c8eda6634e4f3bfbcc9fc8
Slaries <37022995+Slaries@users.noreply.github.com>
2019-06-16 23:06:55 +0300

    WarmUp 1

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -1,4 +1,11 @@
 package warmup;
 
 public class WarmUp1 {
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+        return false;
+    }
 }
