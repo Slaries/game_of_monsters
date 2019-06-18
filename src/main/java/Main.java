ff5937168fa99ba61e4451596dd2c83fb8ff64cb
Slaries <37022995+Slaries@users.noreply.github.com>
2019-06-19 00:34:15 +0300

    warmUp 2 complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import warmup.WarmUp1;
 public class Main {
     public static void main(String[] args) {
         boolean result = WarmUp1.sleepIn (false, false);
+        boolean monkay = WarmUp2.monkeyTrouble(true, true);
     System.out.println(result);
+    System.out.println("Question about monkey " + monkay);
     }
 }
--- src/main/java/warmup/WarmUp2.java
+++ src/main/java/warmup/WarmUp2.java
@@ -1,4 +1,13 @@
 package warmup;
 
 public class WarmUp2 {
+    public static boolean monkeyTrouble(boolean aMonkay, boolean bMonkey) {
+        return (aMonkay && bMonkey) || (!aMonkay && !bMonkey);
+    }
 }
+
+/*
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false
+ */
