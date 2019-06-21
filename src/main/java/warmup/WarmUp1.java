60e96d2303c414240a0d332dda5a463d86d15780
Slaries <slaries@rambler.ru>
2019-06-21 12:18:57 +0400

    задача posNeg  исправлена без boolean result

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -64,11 +64,11 @@ public class WarmUp1 {
     */
 
     public static boolean posNeg(int a, int b, boolean negative) {
-        boolean result = true;
-        if (negative == true) {
-            return result = (a < 0 && b < 0);
+
+        if (negative) {
+            return (a < 0 && b < 0);
         } else {
-            return result = ((a > 0 && b < 0) || (a < 0 && b > 0)) ;
+            return ((a > 0 && b < 0) || (a < 0 && b > 0)) ;
         }
     }
 
