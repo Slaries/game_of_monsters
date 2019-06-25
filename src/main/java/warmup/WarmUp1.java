5acac7408ef4cb7ee6007c3349914137b59c2a41
Slaries <slaries@rambler.ru>
2019-06-25 17:50:52 +0400

    задача icyHot решена

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -157,10 +157,14 @@ public class WarmUp1 {
      */
     public static boolean startHi(String str) {
         int a = str.length();
-        return ((a >= 2)&&("hi".equals(str.substring(0,2))));
-        }
+        return ((a >= 2) && ("hi".equals(str.substring(0, 2))));
     }
 
+    /* Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+     */
 
+    public static boolean icyHot(int temp1, int temp2) {
+        return (temp1 > 100 && temp2 < 0) || (temp1 < 0 && temp2 > 100);
+    }
 
 }
