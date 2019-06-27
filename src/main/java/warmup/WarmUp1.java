ffc2b1019620bac18a520df6b5b6c4c7aaa92d14
Slaries <slaries@rambler.ru>
2019-06-27 09:32:05 +0400

    задача front3 исправлена

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -117,12 +117,14 @@ public class WarmUp1 {
     * Return a new string which is 3 copies of the front.
      */
     public static String front3(String str) {
-        int a = str.length();
+        String result;
+        int a = str.length() ;
         if (3 <= a){
-            return (str.substring(0,3) + str.substring(0,3) + str.substring(0,3));
+            result = str.substring(0,3);
         }else{
-            return (str.substring(0,a) + str.substring(0,a) + str.substring(0,a));
+            result= str;
         }
+        return (result + result + result);
     }
 
     /*Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
