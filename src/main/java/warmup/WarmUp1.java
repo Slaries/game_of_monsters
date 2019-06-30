94e25134a678598fdeea5e5eca4897888577055c
slaries <slaries@rambler.ru>
2019-06-30 21:41:40 +0300

    задача  in3050 решена

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -256,4 +256,13 @@ public class WarmUp1 {
             return result = a;
         }
     }
+    /* Given 2 int values,
+    return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+     */
+    public static boolean in3050(int a, int b) {
+        boolean inRange = (((30 <= a) && (a <= 40)) && (30 <= b) && (b <= 40));
+        boolean outRange = (((40 <= a) && (a <= 50)) && (40 <= b) && (b <= 50));
+        return inRange || outRange;
+    }
+
 }
