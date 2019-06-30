5c7839328dee4e4e6d38c72535580b624b2af15d
slaries <slaries@rambler.ru>
2019-06-30 21:24:26 +0300

    задача in1020 решена

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -169,4 +169,10 @@ public class WarmUp1 {
         return (temp1 > 100 && temp2 < 0) || (temp1 < 0 && temp2 > 100);
     }
 
+    /* Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+     */
+    public boolean in1020(int a, int b) {
+        return (a <= 20 && a >= 10) || (b <= 20 && b >= 10);
+    }
+
 }
