d6ccad7c1f9ad400700452d0a18dfc520b104045
Slaries <slaries@rambler.ru>
2019-07-01 10:32:51 +0400

    задача endUp решена

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -302,6 +302,18 @@ Return true if the given string contains between 1 and 3 'e' chars.
     return counter == 1 || counter == 3 ;
 }
 
+/* Given a string, return a new string where the last 3 chars are now in upper case.
+* If the string has less than 3 chars, uppercase whatever is there.
+* Note that str.toUpperCase() returns the uppercase version of a string.
+*/
+public static String endUp(String str) {
+    int a = str.length();
+    if (a > 3){
+        return str.substring(0,a-3) + str.substring(a-3,a).toUpperCase();
+    }else{
+        return str.toUpperCase();
+    }
+}
 /*
 
  */
