58adf47eace4c5eebbefa9b2f93eba90d7443e19
Slaries <slaries@rambler.ru>
2019-07-01 10:26:17 +0400

    задача intMax исправлена

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -235,9 +235,8 @@ public class WarmUp1 {
         }
         if ((b > a) && (b > c)){
             return b;
-        } else {
-            return c;
         }
+        return c;
     }
     /* Given 2 int values, return whichever value is nearest to the value 10,
     * or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
