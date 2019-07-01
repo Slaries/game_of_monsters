343d412a9605acbb8d7d76a61093db24412133f2
Slaries <slaries@rambler.ru>
2019-07-01 11:51:18 +0400

    задача intMax исправлена

--- src/main/java/warmup/WarmUp1.java
+++ src/main/java/warmup/WarmUp1.java
@@ -243,7 +243,6 @@ public class WarmUp1 {
     public static int intMax(int a, int b, int c) {
         if ((a > b) && (a > c)){
             return a;
-        }else {
         }
         if ((b > a) && (b > c)){
             return b;
