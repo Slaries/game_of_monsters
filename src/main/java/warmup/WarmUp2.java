6b1839ea0f5fba185b2b98df764f186807f6a2c1
Slaries <slaries@rambler.ru>
2019-07-02 17:20:01 +0400

    задача stringTimes решена

--- src/main/java/warmup/WarmUp2.java
+++ src/main/java/warmup/WarmUp2.java
@@ -1,4 +1,11 @@
 package warmup;
 
 public class WarmUp2 {
+    public static String stringTimes(String str, int n) {
+        StringBuilder temp = new StringBuilder("");
+        for (int i = 0; i < n; i++){
+            temp.append(str);
+        }
+        return temp.toString();
+    }
 }
