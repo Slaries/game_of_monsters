dea3c718379e0ae8259961c420a1b3bc07c3d0b3
Slaries <slaries@rambler.ru>
2019-07-04 10:27:22 +0400

    задача arrayFront9 решена

--- src/main/java/warmup/WarmUp2.java
+++ src/main/java/warmup/WarmUp2.java
@@ -80,4 +80,15 @@ public class WarmUp2 {
         }
         return count;
     }
+    /* Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+    * The array length may be less than 4.
+     */
+    public boolean arrayFront9(int[] nums) {
+        boolean result = false;
+        for (int i = 0; i < nums.length;i++){
+            if (nums[i] == 9 && i < 3)
+                result = true;
+        }
+        return result;
+    }
 }
