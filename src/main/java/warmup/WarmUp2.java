1b9e9c1daeeeffacea00cb3cb07b91e48fdb5b7e
slaries <slaries@rambler.ru>
2019-07-05 23:04:39 +0300

    задача stringYak решена

--- src/main/java/warmup/WarmUp2.java
+++ src/main/java/warmup/WarmUp2.java
@@ -158,4 +158,10 @@ public class WarmUp2 {
         }
         return temp;
     }
+    /* Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed,
+    * but the "a" can be any char. The "yak" strings will not overlap.
+     */
+    public static String stringYak(String str) {
+        return str.replaceAll("yak","");
+    }
 }
