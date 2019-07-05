499a33357cfd2f855984e1c9bed0dfdc95559ba5
Slaries <slaries@rambler.ru>
2019-07-05 10:22:59 +0400

    задача array123 решена

--- src/main/java/warmup/WarmUp2.java
+++ src/main/java/warmup/WarmUp2.java
@@ -98,4 +98,21 @@ public class WarmUp2 {
         }
         return result;
     }
+    /* Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+     */
+    public static boolean array123(int[] nums) {
+        int length = nums.length;
+        boolean numbers = false;
+        if (length < 2)
+            return false;
+        for(int i = 0; i < length - 2;i++){
+            if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3)
+                numbers = true;
+        }
+        return numbers;
+    }
+    /*
+
+     */
+
 }
