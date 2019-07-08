8f2fae9dcf3d07e2eb4a92625169fbeac19f0921
Slaries <slaries@rambler.ru>
2019-07-08 16:02:02 +0400

    add 3 test task

--- src/test/java/warmup/WarmUp2Test.java
+++ src/test/java/warmup/WarmUp2Test.java
@@ -10,12 +10,37 @@ public class WarmUp2Test {
     public void countXXTest(){
         //given
         String sourse = "abcxx";
+        //when
         //then
-        //then
-        int expected = 2;
+        int expected = 1;
 
         int actual = WarmUp2.countXX(sourse);
         System.out.println(actual);
         assertEquals(expected,actual);
     }
+
+    @Test
+    public void has271Test(){
+        // given
+        int[] source = {2,7,6,0,10};
+        // when
+
+        //then
+        boolean expected = true;
+        assertEquals(expected, WarmUp2.has271(source));
+    }
+    @Test
+    public void stringMatchTest(){
+        //given
+        String aSource = "xxcaazz";
+        String bSource = "xxbaaz";
+        //when
+
+        //then
+        int expected = 3;
+        assertEquals(expected, WarmUp2.stringMatch(aSource,bSource));
+    }
+
+
+
 }
