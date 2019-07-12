5a0d1f2a7c58e3a6b1b6d79d6c884d04fc589500
slaries <slaries@rambler.ru>
2019-07-12 22:02:09 +0300

    game in text updated

--- src/main/java/fight/Monster.java
+++ src/main/java/fight/Monster.java
@@ -3,8 +3,8 @@ package fight;
 import java.util.Random;
 
 public class Monster {
-   private String name;
-   private int health;
+    private String name;
+    private int health;
 
     public Monster(String name, int health) {
         this.name = name;
@@ -27,18 +27,17 @@ public class Monster {
         return health;
     }
 
-    public void attack(Warrior warrior) {
-        if(this.health > 0) {
+    public int attack(Warrior warrior) {
+        int damage = 0;
+        if (this.health > 0) {
             Random rnd = new Random(System.currentTimeMillis());
             int min = 10;
             int max = 30;
-            int damage = min + rnd.nextInt(max - min + 1);
-            if(warrior.getHealth() <= damage){
-                damage = warrior.getHealth();}
+            damage = min + rnd.nextInt(max - min + 1);
+            if (warrior.getHealth() <= damage) {
+                damage = warrior.getHealth();
+            }
             warrior.setHealth(warrior.getHealth() - damage);
-            System.out.println(this.name + " нанес " + warrior.getName() + " " + damage + " урона, жизни осталось " + warrior.getHealth());
-        }else
-            System.out.println(warrior.getName() + " победил. "+ this.name + " погиб в бою");
-
+        } return damage;
     }
 }
